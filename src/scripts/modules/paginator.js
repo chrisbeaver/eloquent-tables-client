539cbb207e99214ef7d50d6de07521dcec9cea7c
Chris Beaver <chris@chrisbeaver.net>
2016-10-03 17:03:44 -0400

    Starting paginator functionality, tweaks on linter.

--- src/scripts/modules/paginator.js
+++ src/scripts/modules/paginator.js
@@ -1,10 +1,12 @@
-import { paginator, total_count } from '../globals';
+import { paginator, total_count, config } from '../globals';
 /**
  * Fired on AJAX response to construct paginated results.
  *
  */
-    export function buildPaginator()
-    {
-        // console.log('Count: ' + total_count);
-        paginator.innerHTML = total_count.value;
-    }
+export function buildPaginator()
+{
+    // console.log('Count: ' + total_count);
+    paginator.innerHTML = total_count.value;
+    let totalPages = total_count.value / config.tableRows;
+    alert(totalPages);
+}
